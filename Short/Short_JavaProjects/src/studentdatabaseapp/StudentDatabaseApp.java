4b8b1867b3ac478a8b0d305083cfa9c1c72af6ef
Gourav B <pit.wire@pm.me>
2020-06-05 06:55:38 -0400

    setup student constructor for database app

---
+++ Short/Short_JavaProjects/src/studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,17 @@
+package studentdatabaseapp;
+
+import java.util.Scanner;
+
+public class StudentDatabaseApp {
+	
+	
+	public static void main(String[] args) {
+	Student stu1 = new Student();
+	
+	// Ask how many new users we ant to add
+	
+	
+	// Create n number of new students
+
+}
+}
