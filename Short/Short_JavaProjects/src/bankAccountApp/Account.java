1cea44e8f4b5217ef98e300b1aaf68b6ed9a4ffc
Gourav B <pit.wire@pm.me>
2020-06-21 14:34:57 -0400

    implement shared transacts for deposit, withdraw, transfer,      -implement compound interest rate

--- Short/Short_JavaProjects/src/bankAccountApp/Account.java
+++ Short/Short_JavaProjects/src/bankAccountApp/Account.java
@@ -37,7 +37,38 @@ public abstract class Account implements IBaseRate {
 		return lastTwoOfSSN + uniqueID + randomNumber;
 	}
 	
-	// List common methods
+	
+	public void compound() {
+		double accruedInterest = balance * (rate/100);
+		balance = balance + accruedInterest;
+		System.out.println("Accrued Interest: $" + accruedInterest);
+		printBalance();
+	}
+	
+	// List common methods - transactions
+	public void deposit(double amount) {
+		balance = balance + amount;
+		System.out.println("Depositing $" + amount);
+		printBalance();
+	}
+	
+	public void withdraw(double amount) {
+		balance = balance - amount;
+		System.out.println("Withdrawing $" + amount);		
+		printBalance();
+	}
+	
+	public void transfer(String toWhere, double amount) {
+		balance = balance - amount;
+		System.out.println("Transfering $" + amount +  " to " + toWhere);
+		printBalance();
+	}
+	
+	public void printBalance() {
+		System.out.println("Your balance is now: $" + balance);
+	}
+	
+	
 	public void showInfo() {
 		System.out.println(
 				"NAME: " + name +
--- Short/Short_JavaProjects/src/bankAccountApp/BankAccountApp.java
+++ Short/Short_JavaProjects/src/bankAccountApp/BankAccountApp.java
@@ -9,12 +9,22 @@ public class BankAccountApp {
 		Savings savacc1 = new Savings("Richard Doe", "456657897", 2500);
 		
 		// Read a CSV File then create new accounts based on that data
+
 		
 		chkacc1.showInfo();
+		chkacc1.compound();
+
+		System.out.println("***********");
+		savacc1.showInfo();
+		savacc1.compound();
+		// savacc1.deposit(5000);
+		// savacc1.withdraw(200);
+		// savacc1.transfer("Brokerage", 3000);
 		
 		System.out.println("***********");
 		
-		savacc1.showInfo();
+		
+		// Read a CSV File then create new accounts based on that data
 		
 	}
 
