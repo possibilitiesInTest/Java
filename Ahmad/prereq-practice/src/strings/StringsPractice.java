f3f46ddd9c4c405056f8ae41db6e3797962b516f
Gourav B <pit.wire@pm.me>
2020-06-30 15:46:05 -0400

    str.substring      -indexOf      -indexOf(string, startingChar)      -lastIndexOf()      -charAt

---
+++ Ahmad/prereq-practice/src/strings/StringsPractice.java
@@ -0,0 +1,32 @@
+package strings;
+
+public class StringsPractice {
+
+	public static void main(String[] args) {
+
+//		String str1 = "ABCDEFG";
+//		
+//		String extractedString = str1.substring(2);
+//		System.out.println(extractedString);
+//		
+//		if(str1.length() == 5) {
+//			System.out.println("the string length is 5");
+//		} else {
+//			System.out.println("string length is NOT 5");
+//		}
+
+//		String str2 = "Hello yogi there yogi there";
+//		
+//		int a = str2.indexOf("yogi");
+//		int b = str2.lastIndexOf("yogi");
+//		int c = str2.indexOf("there", 7);
+//		
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(c);
+//		System.out.println(str2.charAt(0));
+		
+		
+	}
+
+}
