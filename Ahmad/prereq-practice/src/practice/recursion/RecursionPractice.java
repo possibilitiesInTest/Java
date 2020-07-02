b634634e1f18c038595b97d4f4c52f0340ae42b4
Gourav B <pit.wire@pm.me>
2020-07-02 17:02:11 -0400

    arrays and recursion

---
+++ Ahmad/prereq-practice/src/practice/recursion/RecursionPractice.java
@@ -0,0 +1,18 @@
+package practice.recursion;
+
+public class RecursionPractice {
+
+	public static void main(String[] args) {
+
+		printNumber(4);
+	}
+
+	public static int printNumber(int num) {
+		if(num ==0) {
+			return 0;
+	} else { 
+		System.out.println(num);
+		return printNumber(num-1);
+	}
+	}
+}
