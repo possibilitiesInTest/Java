9b9ed0aa59cf1539b85a89a3b4b4257315d931bb
Gourav B <pit.wire@pm.me>
2020-07-02 03:05:11 -0400

    strings, loops practice

--- Ahmad/assignment_01/src/ignore/TestingUtils.java
+++ Ahmad/assignment_01/src/ignore/TestingUtils.java
@@ -1,32 +1,35 @@
 package ignore;
 
-import assignment_01.App;
+import assignment_02.App;
 
 public class TestingUtils {
 	public static void runTests(){
 		
 
-		String [] params1 = {"12345","apple","tiger","candy","add","ad","a","","del","denny","sfveaadelbb"};
-		String  [] expected = {"234","ppl","ige","and","add","ad","a","","del","enn","aad"};
+		int [] params1 = {60,65,65,80,85,85,70,75,75,40,40,90};
+		
+		boolean[] params2 = { false, false, true, false, false, true, false, false, true, false, true, false };
+
+		int[] expected = { 0, 100, 0, 100, 200, 100, 100, 100, 100, 0, 0, 200 };
         
 		for(int i=0; i < params1.length; i++){
-			String result = App.middleThree(params1[i]);
-			if(result.equals(expected[i])) {
-				System.out.println(printPassResult(params1[i], expected[i], result));
+			int result = App.speedingFine(params1[i], params2[i]);
+			if(result == expected[i]) {
+				System.out.println(printPassResult(params1[i], params2[i], expected[i], result));
 			} else{
-				System.out.println(printFailResult(params1[i], expected[i], result));
+				System.out.println(printFailResult(params1[i], params2[i], expected[i], result));
 			}
 		}
 	}
 	
-	private static String printPassResult(Object params1, Object expected, Object result){
-		return "PASS: middleThree("+ params1.toString()+ ") -> " + result.toString();
+	private static String printPassResult(Object params1, Object params2, Object expected, Object result){
+		return "PASS: speedingFine("+ params1.toString()+ ", "+ params2.toString()+ ") -> " + result.toString();
 	}
 	
 	
-	private static String printFailResult(Object params1, Object expected, Object result){
+	private static String printFailResult(Object params1, Object params2, Object expected, Object result){
 		String ret = "**********************" + "\n";
-		ret += "FAIL: middleThree("+ params1.toString()+ ") -> " + result.toString() 
+		ret += "FAIL: speedingFine("+ params1.toString()+ ", "+ params2.toString()+ ") -> " + result.toString()
 		+ "      Expected: "+ expected.toString();
 		ret += "\n" + "**********************";
 		return ret;
