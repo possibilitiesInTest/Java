21c7348d2773abee520a78cad82c4c9ce338d006
pit-dev <possibilitiesInTest@protonmail.com>
2020-08-02 08:19:24 -0400

    assignments_07,08,09,10 boolean logic examples

---
+++ Ahmad/puzzles/assignment_09/src/assignment_09/App.java
@@ -0,0 +1,66 @@
+package assignment_09;
+
+import ignore.TestingUtils;
+
+public class App {
+
+
+	/**
+
+	Given 2 positive int arguments (a, b), return whichever argument is 
+	nearest to the number 21 without going over.
+	Return 0 if they both go over 21. 
+	<br>
+	<br>
+
+	 * <b>EXPECTATIONS:</b><br>
+		nearestTwentyOne(19, 21)   <b>---></b> 21 <br>
+		nearestTwentyOne(21, 19)    <b>---></b> 21 <br>
+		nearestTwentyOne(19, 22) <b>---></b> 19 <br>
+		nearestTwentyOne(32, 22) <b>---></b> 0 <br>
+	 */
+	
+	public static int nearestTwentyOne(int a, int b) {
+	
+//		if ( a <= 21 && b <= 21) {
+//			if (Math.abs(21-a) > Math.abs(21-b)) {
+//				return b;
+//			} else {
+//				return a;
+//			}
+//		} else if (a <= 21 && b > 21) {
+//			return a;
+//		} else if (b <= 21 && a > 21) {
+//			return b;
+//		} else {
+//		return 0;
+//		}
+		if (a > 21 && b > 21) return 0; // disqualify both
+		if (a > 21 && b <=21) return b; //dsiqualify a
+		if (a <=21 && b > 21) return a; //disqualify b
+		
+		if(a > b) {
+			return a;
+		} else {
+			return b;
+		}
+		
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	//----------------------STARTING POINT OF PROGRAM. IGNORE BELOW --------------------//
+	public static void main(String args[]){
+		TestingUtils.runTests();
+		
+	}
+}
