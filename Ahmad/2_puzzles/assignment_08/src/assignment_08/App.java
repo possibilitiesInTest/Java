031ce8599d675cdef152bbaf0e90036eb6c12e03
possibilitiesInTest <possibilitiesInTest@gmail.com>
2024-02-28 11:47:17 -0500

    update saved files

--- Ahmad/2_puzzles/assignment_08/src/assignment_08/App.java
+++ Ahmad/2_puzzles/assignment_08/src/assignment_08/App.java
@@ -21,12 +21,10 @@ public class App {
 	
 	public static int sumUnique(int a, int b, int c) {
 		
-		
 		if (a == b && b ==c) {return 0;};
 		if ( a == b ) {return c;};
 		if ( a == c ) {return b;};
 		if ( b == c ) {return a;};
-		
 		
 		return a + b + c;
 		
